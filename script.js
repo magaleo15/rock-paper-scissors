0e5dea2977108c9f320425fc87ad2b0d73b76529
Leo M <leomaga.eq@gmail.com>
2024-12-17 12:03:33 -0300

    create getComputerChoice

---
+++ script.js
@@ -0,0 +1,30 @@
+console.log("Hello World");
+
+// computer should randomize their choice
+function getComputerChoice() {
+  let computerNumber = Math.random();
+  let computerChoice = "";
+  if (computerNumber > 0.666) {
+    computerChoice = "paper";
+  } else if (computerNumber < 0.333) {
+    computerChoice = "rock";
+  } else {
+    computerChoice = "scissors";
+  }
+  return computerChoice;
+}
+// user should select their option
+// getHumanChoice
+// game should either
+// user win
+// computer win
+// draw
+// Keep track of score
+// humanScore
+// computerScore
+
+// playRound option
+
+// play 5 rounds option
+
+// options with buttons
