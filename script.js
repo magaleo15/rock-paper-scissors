1aa5e0897dbc8aca26c1feba06dbdb6420df15a4
Leo M <leomaga.eq@gmail.com>
2024-12-17 12:28:28 -0300

    create working playRound POC

--- script.js
+++ script.js
@@ -11,12 +11,18 @@ function getComputerChoice() {
   } else {
     computerChoice = "scissors";
   }
+  console.log(computerChoice);
   return computerChoice;
 }
 
-console.log(getComputerChoice());
+// console.log(getComputerChoice());
 // user should select their option
-// getHumanChoice
+function getHumanChoice() {
+  let humanChoice;
+  humanChoice = prompt("Please select: Rock, Paper or Scissors");
+  console.log(humanChoice.toLowerCase);
+  return humanChoice.toLowerCase();
+}
 // game should either
 // user win
 // computer win
@@ -25,7 +31,48 @@ console.log(getComputerChoice());
 // humanScore
 // computerScore
 
-// playRound option
+// playRound function
+
+function playRound() {
+  let humanChoice = getHumanChoice();
+
+  let computerChoice = getComputerChoice();
+
+  if (humanChoice === computerChoice) {
+    alert(`You both selected ${computerChoice}. DRAW!`);
+  } else if (humanChoice === "rock") {
+    if (computerChoice === "scissors") {
+      alert(
+        `Human selected ${humanChoice}, Computer selected ${computerChoice}. HUMAN WON!`
+      );
+    } else {
+      alert(
+        `Human selected ${humanChoice}, Computer selected ${computerChoice}. COMPUTER WON!`
+      );
+    }
+  } else if (humanChoice === "paper") {
+    if (computerChoice === "scissors") {
+      alert(
+        `Human selected ${humanChoice}, Computer selected ${computerChoice}. HUMAN WON!`
+      );
+    } else {
+      alert(
+        `Human selected ${humanChoice}, Computer selected ${computerChoice}. COMPUTER WON!`
+      );
+    }
+  } else if (humanChoice === "scissors") {
+    if (computerChoice === "paper") {
+      alert(
+        `Human selected ${humanChoice}, Computer selected ${computerChoice}. HUMAN WON!`
+      );
+    } else {
+      alert(
+        `Human selected ${humanChoice}, Computer selected ${computerChoice}. COMPUTER WON!`
+      );
+    }
+  }
+}
+playRound();
 
 // play 5 rounds option
 
